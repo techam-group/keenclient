931438b05a873719ef68240e5b20ee3c416e4614
Favour George C <phavorsparks@gmail.com>
2019-10-01 21:26:34 +0100

    refactor the connect styles

--- src/components/shared/landing/landing-connect/Connect.jsx
+++ src/components/shared/landing/landing-connect/Connect.jsx
@@ -1,51 +1,6 @@
 import React from 'react'
 import { Grid, Typography, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-
-import CommunityImage from '../../../../assets/images/learning-com-table.jpg'
-
-const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-    // width: 100
-  },
-  main: {
-    backgroundImage: `url(${CommunityImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    height: '36rem',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-
-    '&::before': {
-      backgroundColor: 'rgba(0, 0, 0, 0.39)',
-      content: "''",
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      position: 'absolute'
-    },
-
-    '& h3': {
-      color: '#f9f9f9',
-      zIndex: 100
-    },
-
-    '& button': {
-      background: 'white',
-      marginTop: '1.845rem',
-
-      '&:hover': {
-        opacity: '0.8',
-        backgroundColor: 'white'
-      }
-    }
-  }
-}))
+import { useStyles } from '../../../../styles/landing/connect.styles'
 
 export default () => {
   const classes = useStyles()
