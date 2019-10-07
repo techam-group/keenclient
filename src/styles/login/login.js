5c3b5ebdb736cdc064ba8078bca1caef640d6462
Favour George <phavorsparks@gmail.com>
2019-10-07 12:04:43 +0100

    modify login logics

--- src/styles/login/login.js
+++ src/styles/login/login.js
@@ -2,21 +2,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import LoginBGImage from '../../assets/vector/login.jpg'
 
 export const useStyles = makeStyles({
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    height: '100vh',
-    background: '#007991',
-    background: '-webkit-linear-gradient(to right, #78ffd6, #007991)',
-    background: 'linear-gradient(to right, #78ffd6, #007991)'
-  },
-  paper: {
-    width: '60%',
-    height: '65vh',
-    display: 'flex',
-  },
   imageHolder: {
     backgroundImage: `url(${LoginBGImage})`,
     backgroundPosition: 'center center',
