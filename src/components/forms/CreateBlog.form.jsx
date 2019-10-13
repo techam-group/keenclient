33fb40d67598f0751bdb41e7bc42c61ab57b304c
Favour George <phavorsparks@gmail.com>
2019-10-13 12:00:49 +0100

    create blog form styled

--- src/components/forms/CreateBlog.form.jsx
+++ src/components/forms/CreateBlog.form.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import { FormGroup, Grid, Typography, Paper, TextField, FormHelperText, MenuItem, FormControlLabel, Switch, Button, CircularProgress } from '@material-ui/core'
+import { ToastMessage, type } from '../toaster/ToastMessage'
 
 import { useStyles } from '../../styles/createBlog/createBlogForm.styles'
 
@@ -58,144 +59,149 @@ const CreateBlogForm = () => {
   const classes = useStyles()
 
   return (
-    <Formik
-      initialValues={initialState}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting, resetForm }) => {
-        console.log('working tho')
+    <Fragment>
+      <Grid component={Paper} elevation={1} className={classes.paper}>
+        <header>
+          <Typography variant="h5">Create a new blog post</Typography>
+        </header>
 
-        console.log('values are => ', values)
-      }}
-      render={({
-        values: { title, category, isPublished, body, image },
-        errors,
-        touched,
-        handleBlur,
-        isSubmitting,
-        handleChange,
-        handleSubmit
-      }) => (
-          <Grid component={Paper} elevation={1} className={classes.paper}>
-            <header>
-              <Typography variant="h5">Create a new blog post</Typography>
-            </header>
-            <form onSubmit={handleSubmit} autoComplete="off">
-              <div className={classes.formControls}>
+        <Formik
+          initialValues={initialState}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            alert('working tho')
+
+            ToastMessage(type.SUCCESS, 'Created blog successfully')
+
+            console.log('values are => ', values)
+          }}
+          render={({
+            values: { title, category, isPublished, body, image },
+            errors,
+            touched,
+            handleBlur,
+            isSubmitting,
+            handleChange,
+            handleSubmit
+          }) => (
+              <form onSubmit={handleSubmit} autoComplete="off">
+                <div className={classes.formControls}>
+                  <FormGroup>
+                    <TextField
+                      required
+                      fullWidth
+                      type='text'
+                      name="title"
+                      value={title}
+                      margin="normal"
+                      label="Blog Title"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      placeholder="Blog Title"
+                      error={errors.title && touched.title}
+                    />
+                    {
+                      errors.title && touched.title && (
+                        <FormHelperText className={classes.errorFeedback}>{errors.title}</FormHelperText>
+                      )
+                    }
+                  </FormGroup>
+                  <FormGroup>
+                    <TextField
+                      select
+                      fullWidth
+                      margin="normal"
+                      name="category"
+                      value={category}
+                      onBlur={handleBlur}
+                      label="Blog Category"
+                      placeholder="Category"
+                      onChange={handleChange}
+                      error={errors.category && touched.category}
+                      SelectProps={{
+                        MenuProps: {
+                          className: classes.menu,
+                        },
+                      }}
+                    >
+                      {
+                        categories.map(option => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))
+                      }
+                    </TextField>
+                    {
+                      errors.category && touched.category && (
+                        <FormHelperText className={classes.errorFeedback}>{errors.category}</FormHelperText>
+                      )
+                    }
+                  </FormGroup>
+                </div>
                 <FormGroup>
                   <TextField
-                    required
                     fullWidth
-                    type='text'
-                    name="title"
-                    value={title}
+                    type="text"
+                    name="image"
+                    value={image}
                     margin="normal"
-                    label="Blog Title"
                     onBlur={handleBlur}
                     onChange={handleChange}
-                    placeholder="Blog Title"
-                    error={errors.title && touched.title}
+                    placeholder="Enter image url"
+                    error={errors.image && touched.image}
                   />
                   {
-                    errors.title && touched.title && (
-                      <FormHelperText className={classes.errorFeedback}>{errors.title}</FormHelperText>
+                    errors.image && touched.image && (
+                      <FormHelperText className={classes.errorFeedback}>{errors.image}</FormHelperText>
                     )
                   }
                 </FormGroup>
                 <FormGroup>
-                  <TextField
-                    select
-                    fullWidth
-                    margin="normal"
-                    name="category"
-                    value={category}
-                    onBlur={handleBlur}
-                    label="Blog Category"
-                    placeholder="Category"
-                    onChange={handleChange}
-                    error={errors.category && touched.category}
-                    SelectProps={{
-                      MenuProps: {
-                        className: classes.menu,
-                      },
-                    }}
-                  >
-                    {
-                      categories.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        color="primary"
+                        value="checkedB"
+                        checked={isPublished}
+                        onChange={handleChange('checkedB')}
+                      />
                     }
-                  </TextField>
+                    label="Publish Now"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <TextField
+                    rowsMax='4'
+                    multiline
+                    name='body'
+                    value={body}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
                   {
-                    errors.category && touched.category && (
-                      <FormHelperText className={classes.errorFeedback}>{errors.category}</FormHelperText>
+                    errors.body && touched.body && (
+                      <FormHelperText className={classes.errorFeedback}>{errors.body}</FormHelperText>
                     )
                   }
                 </FormGroup>
-              </div>
-              <FormGroup>
-                <TextField
+
+                <div className={classes.spacing} />
+
+                <Button
                   fullWidth
-                  type="text"
-                  name="image"
-                  value={image}
-                  margin="normal"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  placeholder="Enter image url"
-                  error={errors.image && touched.image}
-                />
-                {
-                  errors.image && touched.image && (
-                    <FormHelperText className={classes.errorFeedback}>{errors.image}</FormHelperText>
-                  )
-                }
-              </FormGroup>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Switch
-                      color="primary"
-                      value="checkedB"
-                      checked={isPublished}
-                      onChange={handleChange('checkedB')}
-                    />
-                  }
-                  label="Publish Now"
-                />
-              </FormGroup>
-              <FormGroup>
-                <TextField
-                  rowsMax='4'
-                  multiline
-                  name='body'
-                  value={body}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {
-                  errors.body && touched.body && (
-                    <FormHelperText className={classes.errorFeedback}>{errors.body}</FormHelperText>
-                  )
-                }
-              </FormGroup>
-
-              <div className={classes.spacing} />
-
-              <Button
-                fullWidth
-                type="submit"
-                variant="contained"
-                className={classes.button}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? <CircularProgress /> : 'Create Post'}
-              </Button>
-            </form>
-          </Grid>
-        )}
-    />
+                  type="submit"
+                  variant="contained"
+                  className={classes.button}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? <CircularProgress /> : 'Create Post'}
+                </Button>
+              </form>
+            )}
+        />
+      </Grid>
+    </Fragment>
   )
 }
 
