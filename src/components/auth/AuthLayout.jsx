6825c5709690b5d2b9e4e128369d378d69368c7d
Favour George <phavorsparks@gmail.com>
2020-01-15 23:35:07 +0100

    add general layout

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core'
-import PlainFooter from '../shared/footer/PlainFooter'
+import { Grid, Paper } from '@material-ui/core';
+import PlainFooter from '../shared/footer/PlainFooter';
 
-import { useStyles } from '../../styles/authLayout/authLayout.styles'
+import { useStyles } from '../../styles/authLayout/authLayout.styles';
 
 const AuthLayout = ({children, ...props}) => {
-	const classes = useStyles(props)
+	const classes = useStyles(props);
 
 	return (
 		<Grid container className={classes.root}>
