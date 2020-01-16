9bdf6a8a2f07f9ce09b53874f184b2a08a98c0e7
Favour George <phavorsparks@gmail.com>
2020-01-16 01:10:42 +0100

    add tutorials and video pages

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Blog from "../views/Blog";
 import Home from '../views/Home';
 import Login from '../views/Login';
+import Video from "../views/Video";
 import Signup from '../views/Signup';
 import NotFound from '../views/NotFound';
+import Tutorials from "../views/Tutorials";
 import Dashboard from '../views/Dashboard';
 import PrivateRoute from './auth/PrivateRoute';
 import EmailVerification from '../views/EmailVerification';
@@ -18,7 +20,9 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/video" component={Video} />
         <Route exact path="/sign-up" component={Signup} />
+        <Route exact path="/tutorials" component={Tutorials} />
         <Route exact path="/verify-email" component={EmailVerification} />
         {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
         <Route path="/dashboard" component={Dashboard} />
